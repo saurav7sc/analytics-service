ae1e6ca508d8420ac3d6ef022f14fd55ad5d94dd
saurav7sc <saurav7.ac@gmail.com>
2025-12-14 00:20:53 +0530

    added EventProcessor

---
+++ service/src/main/java/com/analytics/processor/EventProcessor.java
@@ -0,0 +1,31 @@
+package com.analytics.processor;
+
+import com.analytics.model.Event;
+import com.analytics.repository.ActiveUsersRepository;
+import com.analytics.repository.PageViewRepository;
+import com.analytics.repository.SessionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EventProcessor {
+
+    private final ActiveUsersRepository activeUsersRepository;
+    private final SessionRepository sessionRepository;
+    private final PageViewRepository pageViewRepository;
+
+    @Autowired
+    public EventProcessor(ActiveUsersRepository activeUsersRepository,
+                          SessionRepository sessionRepository,
+                          PageViewRepository pageViewRepository) {
+        this.activeUsersRepository = activeUsersRepository;
+        this.sessionRepository = sessionRepository;
+        this.pageViewRepository = pageViewRepository;
+    }
+
+    public void process(Event event) {
+        activeUsersRepository.recordActivity(event.getUserId(), event.getTimestamp());
+        sessionRepository.recordSession(event.getUserId(), event.getSessionId(), event.getTimestamp());
+        pageViewRepository.recordPageView(event.getPageUrl(), event.getTimestamp());
+    }
+}
